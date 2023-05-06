21376de865d5220f68b4f37d58d361bedb65c830
Igbafesamuel <92268748+igbafes@users.noreply.github.com>
2023-05-06 07:20:20 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+const button = document.querySelector('button');
+const body = document.querySelector ('body');
+
+   button.addEventListener('click',() => {
+    body.classList.toggle('dark');
+     
+  });
